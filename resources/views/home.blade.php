feabb520ad3ac596122a5c2f79f433bc9ace89de
FDXNRFXI <randomfilm12@gmail.com>
2022-08-16 07:49:27 +0700

    Tugas PBO First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko diamond Murah</title>
+</head>
+<body>
+<h1> Topup Diamond murah cek website [ virrzstore.com ]</h1>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
